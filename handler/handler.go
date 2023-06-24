9008d9c04dfa00bead210636c7c9e4d028365fc9
Evans Owamoyo <lordvidex.prime@gmail.com>
2023-06-25 01:12:27 +0300

    feat: added logger

--- handler/handler.go
+++ handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"github.com/lordvidex/errs"
@@ -56,6 +57,7 @@ func (h *Handler) Start(port string) error {
 
 func (h *Handler) setup() {
 	r := h.router
+	r.Use(middleware.Logger)
 	// Public routes
 	r.Group(func(r chi.Router) {
 		r.Get("/health", h.health)
